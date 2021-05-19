3f065fe12b599acd07fb64406bf1909e979af274
RealYaddie <alphaleo777@gmail.com>
2021-05-19 00:52:31 -0500

    Added some more onto the Final report doc.

--- Python Files/Weld_Progam_Code_3.py
+++ Python Files/Weld_Progam_Code_3.py
@@ -17,31 +17,33 @@ robot = RDK.Item('ABB IRB 1200-5/0.9')
 home = RDK.Item('Home')
 target = RDK.Item('Target 1')
 
-# From the [INSERT NAME OF MODULE] module we get the position of the target in space
+# Using the Robolink module we are able to get the position of the target in space
+# from the RoboDK simulation environment
 # in the form of a 4x4 matrix and then we save this to a variable
 # get the pose of the target in the form of a 4x4 matrix <- REMOVE
 poseref = target.Pose()
 
-# Using the [INSERT NAME OF MODULE] module we first move the robot arm to the 'Home' position
+# Using the Robolink module we first move the robot arm to the 'Home' position
 # and then to the 'Target 1' position
 # move the robot to the home position, and then to the center <- REMOVE
 robot.MoveJ(home)
 robot.MoveJ(target)
 
-# Using the [INSERT NAME OF MODULE] moduele we're able to use a message box
+# From the RoboDK module we're able to use a message box
 # to collect the desired amount of vertices the shape should have from the user
 # This can be used to make different shapes including a circle, triangle, square etc.
 # Using message box to get number of vertices of the shape <- REMOVE
 num_vertices = mbox("Enter then number of vertices", entry=True)
 num_vertices = int(num_vertices)
 
-# Using the [INSERT NAME OF MODULE] module we're able to 
+# Using both the RoboDK and the Robolink  module we're able to 
 # make a hexagon shape around the center of the selected target:
 for i in range(num_vertices+1):
     ang = i*2*pi/num_vertices      # angle: 0, 60, 120, ..., 360
     posei = poseref*rotz(ang)*transl(300, 0, 0)*rotz(-ang)
     robot.MoveL(posei)
 
-# move back to the center, and then back to the home position
+# Finally we move the robot arm back to the target position and then to the home
+# position and the program ends here.
 robot.MoveL(target)
 robot.MoveJ(home)
